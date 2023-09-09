1fef250b3a09109f661843c17b66c425d97f113f
codewithheeren <heeren.tech@gmail.com>
2023-09-09 17:41:57 +0530

    hello

---
+++ workspace/nestjs6/src/employee/EmployeePipe.ts
@@ -0,0 +1,29 @@
+import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class EmployeePipe implements PipeTransform {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    const { metatype } = metadata;
+
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      const errorMessage = Object.values(errors[0].constraints).join(', ');
+      throw new BadRequestException(errorMessage);
+    }
+
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
